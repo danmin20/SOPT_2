098bd98e21b43bb278a5e80a45473b9e2fbff0a7
leejeongmin <danmin20@khu.ac.kr>
2020-05-01 23:49:38 +0900

    gridlayoutmanager

--- app/src/main/java/com/danmin/sopt_2/Book/BookData.kt
+++ app/src/main/java/com/danmin/sopt_2/Book/BookData.kt
@@ -1,6 +1,6 @@
 package com.danmin.sopt_2.Book
 
 data class BookData(
-    val bookTitle: String,
-    val img_book: Int
+    val title: String,
+    val book_img: String
 )
--- app/src/main/java/com/danmin/sopt_2/LibraryFragment.kt
+++ app/src/main/java/com/danmin/sopt_2/LibraryFragment.kt
@@ -5,10 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import com.danmin.sopt_2.Book.BookAdapter
 import com.danmin.sopt_2.Book.BookData
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_library.*
 
 class LibraryFragment : Fragment() {
@@ -22,30 +21,38 @@ class LibraryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_library, container, false)
     }
 
-    val img = arrayOf(
-        R.drawable.ai,
-        R.drawable.css,
-        R.drawable.html,
-        R.drawable.id,
-        R.drawable.jpg,
-        R.drawable.js,
-        R.drawable.mp4,
-        R.drawable.pdf,
-        R.drawable.png,
-        R.drawable.psd,
-        R.drawable.tiff,
-        R.drawable.php
-    )
-
-    val text = arrayOf(
-        "ai", "css", "html", "id", "jpg", "js", "mp4", "pdf", "png", "psd", "tiff", "php"
-    )
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bookAdapter = BookAdapter(view.context, img, text)
-        gridview.adapter = bookAdapter
+        bookAdapter = BookAdapter(view.context)
+        rv_book.run {
+            adapter = bookAdapter
+            layoutManager = GridLayoutManager(view.context, 3)
+        }
+        loadDatas()
+    }
+
+    private fun loadDatas() {
+        datas.apply {
+            add(
+                BookData(
+                    title = "이정민",
+                    book_img = "https://avatars1.githubusercontent.com/u/50590192?s=460&u=e6a104455a4e61cb4b4693e1be8debc48bb955f6&v=4"
+                )
+            )
+            add(
+                BookData(
+                    title = "이정민",
+                    book_img = "https://avatars1.githubusercontent.com/u/50590192?s=460&u=e6a104455a4e61cb4b4693e1be8debc48bb955f6&v=4"
+                )
+            )
+            add(
+                BookData(
+                    title = "이정민",
+                    book_img = "https://avatars1.githubusercontent.com/u/50590192?s=460&u=e6a104455a4e61cb4b4693e1be8debc48bb955f6&v=4"
+                )
+            )
+        }
+        bookAdapter.datas = datas
+        bookAdapter.notifyDataSetChanged()
     }
 }
-
-
